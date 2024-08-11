ddc659226f551e4112b388b8d9d4a38133bac030
KadirKenan <kadirkenan.kk@gmail.com>
2024-08-11 19:54:17 +0300

    Version 2.0.1

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const server = http.createServer((req, res) => {
     console.log(`Sayfa yenilendi: ${now.toLocaleString()}`);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World111\n');
+    res.end('Hello World112131231\n');
 });
 
 const PORT = 3000;
